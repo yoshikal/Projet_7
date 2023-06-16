d24d67d6283d8855dd7834b4109d7984579a5d7a
yoshikal <57770904+yoshikal@users.noreply.github.com>
2023-06-16 23:47:50 +0200

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -2,4 +2,4 @@
 apt update
 apt-get install -y libgomp1
 gunicorn -k uvicorn.workers.UvicornWorker main:app
-streamlit run stream_lit.py --server.baseUrlPath=/https://scoringmodeloc.azurewebsites.net/
+streamlit run stream_lit.py --server.baseUrlPath=https://scoringmodeloc.azurewebsites.net/
